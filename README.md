a1ef7999fc91e77dbe3c1dfd07c2cf3e4701a562
Adam Isaac <aci747@engineer.com>
2019-03-24 16:35:23 -0400

    Add iptables lab

---
+++ README.md
@@ -0,0 +1,11 @@
+## CSG Labs
+This repo contains lab scripts and docs for the CCDC virtual enviroment & lab area.
+
+PRs/commits welcome.
+
+All labs should have the following:
+
+* A lab manual (MANUAL.md or other format)
+* Completion check script (checkcomplete.sh or other format)
+* SysAdmin setup instructions and/or topology for the enviroments (setup.txt)
+* Other supplimental docs (PowerPoints, videos, whatever) in an open format  
---
+++ linux/iptables/completecheck.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# This script judges completion of the iptables lab via cURL
+# See the lab manual for more details
+if [ -z "$(command -v curl)" && -z "$(command -v nc)"]; then
+    if [[ $# -eq 1 && $1 =~ [0-9.] ]]; then
+        echo 'Checking connection every 10 seconds...'
+        while [ true ]; do
+            sleep 10
+            curl -I -s $1 --connect-timeout 5 > /dev/null
+            if [[ $? -eq 28 ]]; then #Timeout
+                nc -z -w5 $1 8080 # Can I connect to any ports at all?
+                if [[ $? -eq 0 ]]; then # Yes!
+                  echo 'Lab complete. Connection blocked to port 80'
+                  exit
+                else # No!
+                  echo 'Lab complete. Connection blocked to server'
+                  exit
+                fi
+            fi
+            if [[ $? -eq 35 ]]; then # TCP reset
+                echo 'Lab complete. Connection rejected with TCP reset!'
+                exit
+            fi
+        done
+    else
+        echo 'Please include ONLY THE IP of the webserver as an argument'
+        exit 2
+    fi
+else
+    echo 'You must have curl and netcat (nc) installed and in your path to run this script!'
+    exit 1
+fi
